323a63311e70bf31cef7f4b4f2dde65ab2ff3b8e
Maximilian Berger <max.berger@evalu.io>
2017-01-04 02:19:38 +0100

    first version

---
+++ pysparkasse.py
@@ -0,0 +1,43 @@
+import csv
+
+def strip_long_spaces(input):
+    loc = input.find("  ")
+    if loc > 0:
+        return input[:loc]
+    return input
+
+csv.register_dialect('sparkasse', delimiter=';', quoting=csv.QUOTE_ALL)
+csv.register_dialect('ynab', delimiter=",", quoting=csv.QUOTE_MINIMAL)
+
+with open('trans.csv') as csvfile:
+    reader = csv.DictReader(csvfile, dialect="sparkasse")
+
+    buff = []
+
+    for row in reader:
+        # Replace , with .
+        row["Betrag"] = str.replace(row["Betrag"], ",", ".")
+
+        # Replace long payee
+        row["Beguenstigter/Zahlungspflichtiger"] = strip_long_spaces(row["Beguenstigter/Zahlungspflichtiger"])
+
+        buff.append(row)
+
+    with open("out.csv", "wb") as csvout:
+        fieldnames_ynab = ["Date","Payee","Category","Memo","Outflow","Inflow"]
+        writer = csv.DictWriter(csvout, dialect="ynab", fieldnames=fieldnames_ynab)
+        writer.writeheader()
+        for b in buff:
+            entry = {}
+            entry["Date"] = b["Buchungstag"]
+            entry["Payee"] = b["Beguenstigter/Zahlungspflichtiger"]
+            entry["Memo"] =  b["Verwendungszweck"]
+
+            # parse value
+            betrag = b["Betrag"]
+            if (betrag.startswith("-")):
+                entry["Outflow"] = betrag[1:]
+            else:
+                entry["Inflow"] = betrag
+
+            writer.writerow(entry)
